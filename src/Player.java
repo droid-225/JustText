220174f9c40226592cace4b5943224ee6d401bd8
Rishit Shah <aryanrishit1@gmail.com>
2025-09-05 16:02:13 +0530

    Commit 6: Removed all the old junk.

--- src/Player.java
+++
@@ -1,9 +0,0 @@
-public class Player extends Entity {
-    private String name = "";
-
-    Player(String name, int age) {
-        this.name = name;
-        setAge(age);
-        setLifespan(100);
-    }
-}
