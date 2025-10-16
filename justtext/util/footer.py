a1a8e8374bebb403ec2af1d31f3906159021932f
Rishit Shah <aryanrishit1@gmail.com>
2025-10-16 23:44:36 +0530

    Commit 80: Placed stamina counter in center of footer.

--- justtext/util/footer.py
+++ justtext/util/footer.py
@@ -1,10 +1,14 @@
 from ..constants import WHITE, BLACK
 from ..assets import load_font
+from .text import TextRenderer
+from ..state import get_state
 
 class Footer:
     def __init__(self, surface):
         self.font = load_font()
         self.surface = surface
+        self.text = TextRenderer(self.font)
+        self.state = get_state()
         
     def draw(self):
         surface = self.surface
@@ -12,6 +16,10 @@ class Footer:
         # Inventory Screen
         inv = self.font.render("(I) Inventory", True, WHITE, BLACK)
         surface.blit(inv, (10, surface.get_height() - inv.get_height() - 6))
+
+        # Stamina Counter
+        stamina = self.font.render(str(self.state.stamina), True, WHITE, BLACK)
+        surface.blit(stamina, ((surface.get_width() / 2) - stamina.get_width() + 20, surface.get_height() - inv.get_height() - 6))
 
         # Stats Screen
         stats = self.font.render("(U) Stats", True, WHITE, BLACK)
