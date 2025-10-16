64bb6772b2d80eaf3159b412715f48d9e6928ff0
Rishit Shah <aryanrishit1@gmail.com>
2025-10-16 23:56:15 +0530

    Commit 85: Added bread to item list, implementation reqruied.

--- justtext/items.py
+++ justtext/items.py
@@ -13,5 +13,6 @@ class ItemDef:
 
 ITEMS: dict[str, ItemDef] = {
     "stone": ItemDef(id="stone", name="Stone", stackable=True, value=1, type="material", rarity="common"),
+    "bread": ItemDef(id="bread", name="Bread", stackable=True, value=1, type="consumable", rarity="common"),
     "pickaxe": ItemDef(id="pickaxe", name="Pickaxe", stackable=False, value=0, type="tool", rarity="common")
 }
