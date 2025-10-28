6eb73ec761ea4ab3734b9b4695bf88cd114b8919
Rishit Shah <aryanrishit1@gmail.com>
2025-10-28 18:20:35 +0530

    Commit 144: Added a consumable effect to the bread item.

--- justtext/items.py
+++ justtext/items.py
@@ -2,22 +2,6 @@ from dataclasses import dataclass
 from typing import Literal, Optional, Callable
 from state import GameState
 
-@dataclass(frozen=True)
-class ItemDef:
-    id: str
-    name: str
-    stackable: bool
-    value: int = 0
-    type: Literal["material", "tool", "consumable", "armor"] = "material"
-    rarity: Literal["common", "mystic", "rare", "ultra rare", "legendary", "mythical", "godly"] = "common"
-    # TODO: add build quality qualifers for tools and armor
-
-ITEMS: dict[str, ItemDef] = {
-    "stone": ItemDef(id="stone", name="Stone", stackable=True, value=1, type="material", rarity="common"),
-    "bread": ItemDef(id="bread", name="Bread", stackable=True, value=1, type="consumable", rarity="common"),
-    "pickaxe": ItemDef(id="pickaxe", name="Pickaxe", stackable=False, value=0, type="tool", rarity="common")
-}
-
 @dataclass
 class ConsumableEffect:
     # Defines what happens when an itemm is used
@@ -31,3 +15,36 @@ class Consumable:
         # Apply consumable effects and return result message
         if self.effect.stamina:
             state.stamina = min(100, state.stamina + self.effect.stamina)
+
+@dataclass(frozen=True)
+class ItemDef:
+    id: str
+    name: str
+    stackable: bool
+    value: int = 0
+    type: Literal["material", "tool", "consumable", "armor"] = "material"
+    rarity: Literal["common", "mystic", "rare", "ultra rare", "legendary", "mythical", "godly"] = "common"
+    # TODO: add build quality qualifers for tools and armor
+    consumable_effect: ConsumableEffect | None = None
+
+ITEMS: dict[str, ItemDef] = {
+    "stone": ItemDef(id="stone", 
+                     name="Stone", 
+                     stackable=True, 
+                     value=1, 
+                     type="material", 
+                     rarity="common"),
+    "bread": ItemDef(id="bread", 
+                     name="Bread", 
+                     stackable=True, 
+                     value=1, 
+                     type="consumable", 
+                     rarity="common", 
+                     consumable_effect=ConsumableEffect(stamina=10)),
+    "pickaxe": ItemDef(id="pickaxe", 
+                       name="Pickaxe", 
+                       stackable=False, 
+                       value=0, 
+                       type="tool", 
+                       rarity="common")
+}
